a0f094f2606cf68f7233877ae61bddf66a44e398
bluemoosegoose <104439807+bluemoosegoose@users.noreply.github.com>
2022-04-26 10:40:03 -0400

    Add files via upload

---
+++ A_Delete Dockscrap.sh
@@ -0,0 +1,17 @@
+﻿#!/bin/bash
+
+#Set Variable for Current User
+currentuser=$(/bin/ls -la /dev/console | /usr/bin/cut -d ' ' -f 4)
+echo currentuser-variable = $currentuser
+
+#Check to see if Dock has been built already
+dockscrap=/Users/$currentuser/dockscrap.txt
+echo "The dockscrap file is set to" $dockscrap
+
+if [ -f $dockscrap ]; then
+    echo "The dockscrap file exists. Deleting the file." 
+    rm $dockscrap
+    exit 0
+fi
+
+exit 0
